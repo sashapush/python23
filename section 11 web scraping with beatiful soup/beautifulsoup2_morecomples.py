8cb20f407e9348c1515a424c2e384050530872bb
Alexander <sashapush@tut.by>
2024-06-11 17:44:24 +0200

    moved soup functions to class

--- section 11 web scraping with beatiful soup/beautifulsoup2_morecomples.py
+++ section 11 web scraping with beatiful soup/beautifulsoup2_morecomples.py
@@ -1,3 +1,4 @@
+import re
 from bs4 import BeautifulSoup
 
 
@@ -34,8 +35,6 @@ ITEM_HTML = '''<html><head></head><body>
 '''
 
 soup = BeautifulSoup(ITEM_HTML,'html.parser')
-
-
 def find_item_name():
 #item name
 #path of children up
@@ -48,10 +47,32 @@ def find_item_name():
 
 def find_item_link():
     locator = 'article.product_pod a'  # css locator
-    item_link = soup.select_one(locator)  #
-    link = item_link.attrs['href']  # access the title property of string above
-    print(link)
+    item_link = soup.select_one(locator).attrs['href'] #relative link wo domain name
+    print(item_link)
 
+def find_item_price():
+    #my way
+    locator = 'article.product_pod p.price_color'  # css locator
+    expression = '£([0-9]+\.[0-9]+)' #() is a group; + is 1 or more (I've used * and it's 0 or more
+    item_price = soup.select_one(locator).getText() #=£51.77 #lector did .string instead of .getText()
+    matches = re.search(expression, item_price)
+    print(matches.group(0)) #£51.77 (entire match)
+    print(float(matches.group(1)))  #51.77 first thing in a brackets
+    #lector way (if different)
+
+def find_item_rating():
+    locator = 'article.product_pod p.star-rating'  # css locator; star-rating Three - 2 css classes
+    star_rating_tag = soup.select_one(locator)
+    classes = star_rating_tag.attrs["class"] #we get list of values ['star-rating', 'Three']
+    #we need to find out a class which is not 'star-rating'; list comprehension way
+    rating_classes = [r for r in classes if r !='star-rating']
+    # filter function way
+    #rating_classes = filter(lambda x: x!='star-rating', classes) #TODO podumoi tut
+    #item_rating
+    print(classes)
+    print(rating_classes[0]) #to return the element, not the full list
 
 find_item_name()
 find_item_link()
+find_item_price()
+find_item_rating()
