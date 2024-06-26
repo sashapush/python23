fec778fe7a084eebc51d323efc29b989358a607b
Alexander <sashapush@tut.by>
2024-06-26 17:53:58 +0200

    Books page

---
+++ section 11 web scraping with beatiful soup/5_scraping books/pages/all_books_page.py
@@ -0,0 +1,9 @@
+#takes in entire html content and parses with beautiful soup
+from bs4 import BeautifulSoup #external imports go above
+from locators.all_books_page import AllBooksPageLocators #internal below. Just a convention
+class AllBooksPage:
+    def __init__(self, page_content):
+        self.soup = BeautifulSoup(page_content, 'html.parser')
+    @property
+    def books(self):
+        return [BookParser(e) for e in self.soup.select(AllBooksPageLocators.BOOKS)] #not selectone, select
