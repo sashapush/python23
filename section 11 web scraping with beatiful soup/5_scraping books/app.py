d13d5bc6d147f6d63a5549ae72fb52aaabbe93aa
Alexander <sashapush@tut.by>
2024-07-04 18:12:48 +0200

    added parsing of 50 hardcoded pages. to do - not hardcode 50

--- section 11 web scraping with beatiful soup/5_scraping books/app.py
+++ section 11 web scraping with beatiful soup/5_scraping books/app.py
@@ -1,16 +1,17 @@
 import requests
 from pages.all_books_page import AllBooksPage
+#this way we scrape 1 page
+page_content = requests.get("https://books.toscrape.com/").content #we get the content of only 1 page out of 50
 
-page_content = requests.get("https://books.toscrape.com/").content #we get the content
 page = AllBooksPage(page_content)
 
 books = page.books #property of AllBooksPage class
 # for book in books:
 #     print(book)
 
-#sort the books by rating and price
-
-
-
-
-#todo add menu
+#this way we scrape all the 50 pages
+for page_num in range(1,50): #1-49 (we have 1st page above)
+    url = f"https://books.toscrape.com/catalogue/page-{page_num+1}.html"
+    page_content = requests.get(url).content
+    page = AllBooksPage(page_content)
+    books.extend(page.books) #extend() is used for many elements, append() for 1
