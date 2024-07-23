6d412fbcd87e9be0228889bf817350d6baecd8ec
Alexander <sashapush@tut.by>
2024-07-23 20:51:27 +0200

    - Added try except exception catching.

--- section 12 selenium/4_scraping quotes selenium/app.py
+++ section 12 selenium/4_scraping quotes selenium/app.py
@@ -1,13 +1,19 @@
 from selenium import webdriver
-from pages.quotes_page import QuotesPage
+from pages.quotes_page import QuotesPage, InvalidTagForAuthorError
 
 #authors = page.get_authors()
-author = input(f"Enter the author you'd like quotes from the list: ")
-tag = input("Enter your tag: ")
+try:
+    author = input(f"Enter the author you'd like quotes from the list: ")
+    tag = input("Enter your tag: ")
 
-chrome = webdriver.Chrome()
-chrome.get("https://quotes.toscrape.com/search.aspx")
-#chrome.get("https://quotes.toscrape.com/")
-page = QuotesPage(chrome) #and give the content to QuotesPage where constructor, which parses html to get self.soup
+    chrome = webdriver.Chrome()
+    chrome.get("https://quotes.toscrape.com/search.aspx")
+    #chrome.get("https://quotes.toscrape.com/")
+    page = QuotesPage(chrome) #and give the content to QuotesPage where constructor, which parses html to get self.soup
 
-print(page.search_for_quotes(author, tag))
+    print(page.search_for_quotes(author, tag))
+except InvalidTagForAuthorError as e:
+    print(e)
+except Exception as e:
+    print(e)
+    print("An unknown error occured. Please try again")
