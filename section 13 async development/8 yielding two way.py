87c18c72d2ef3542507f1f07b5b058a441020f90
Alexander <sashapush@tut.by>
2024-08-15 11:54:51 +0200

    - Yielding in two generators

---
+++ section 13 async development/8 yielding two way.py
@@ -0,0 +1,24 @@
+from collections import deque
+##DEQUE double-ended queue
+#Useful for operations with threads, see asynchronous development
+friends = deque(("Rolf","Jose","Charlie","Jen","Anna"))
+
+def get_friend(): #generator
+    yield from friends #
+
+# c = get_friend()
+# print(next(c))
+# print(next(c))
+
+def greet(g): #also generator
+    while True:
+        try:
+            friend = next(g)
+            yield f"Hello {friend}"
+        except StopIteration:
+            pass
+
+friends_generator = get_friend() #on this generator we can call next()
+g = greet(friends_generator)  #we are giving above generator to greet()
+print(next(g))
+print(next(g))
