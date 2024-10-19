a974956b580ea4fa64e5650cf5e90da173f8f351
Alexander <sashapush@tut.by>
2024-10-19 14:07:19 +0200

    The basic Flask page using pipenv virtual env, + new endpoint to show error page - created base jinja file and extended is to 404 and post pages. - adapted code to use .jinja files

--- Section 15 flask app/app.py
+++ Section 15 flask app/app.py
@@ -14,9 +14,11 @@ def home():
     return "Hello, uwu!"
 
 @app.route('/post/<int:post_id>') #crocodile clips, int is replaced by number of post_id. Flask unique syntax
-def post(post_id):
-    #post = posts.get(post_id)
-    return render_template('post.html', post=posts.get(post_id)) #we pass post argument from post.html
+def post(post_id): #better not to name function as variable and vice versa
+    post = posts.get(post_id)
+    if not post: # if post not found - None if not found; not None => True
+        return render_template('404.jinja2', message=f"A post with id {post_id} was not found.")
+    return render_template('post.jinja2', post=posts.get(post_id)) #we pass post argument from post.jinja2
     #f"Post is: {post['title']}, content:\n\n{post['content']}"
 
 
