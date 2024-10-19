42b044f63dec78b109a8a560f335ca1d1c0b8dd3
Alexander <sashapush@tut.by>
2024-10-19 12:40:35 +0200

    The basic Flask page using pipenv virtual env, + new endpoint to read hardcoded posts

--- Section 15 flask app/app.py
+++ Section 15 flask app/app.py
@@ -2,9 +2,22 @@ from flask import Flask
 
 app = Flask(__name__) #__name__ is always unique
 
+posts = {
+    0:{
+        'title': 'a title',
+        'content': 'This is again the first one'
+    }
+}
+
 @app.route('/')
 def home():
     return "Hello, uwu!"
+
+@app.route('/post/<int:post_id>') #crocodile clips, int is replaced by number of post_id. Flask unique syntax
+def post(post_id):
+    post = posts.get(post_id)
+    return f"Post is: {post['title']}, content:\n\n{post['content']}"
+
 
 
 if __name__ == "__main__":
