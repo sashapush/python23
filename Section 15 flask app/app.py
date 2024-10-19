7105988b4419a15109cad7d1f3711181df655a33
Alexander <sashapush@tut.by>
2024-10-19 12:33:37 +0200

    The basic Flask page using pipenv virtual env

--- Section 15 flask app/app.py
+++ Section 15 flask app/app.py
@@ -1,1 +1,11 @@
 from flask import Flask
+
+app = Flask(__name__) #__name__ is always unique
+
+@app.route('/')
+def home():
+    return "Hello, uwu!"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
