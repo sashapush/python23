22f5da85d7ec3ae63841bf47bd71debc9c8c3270
Alexander <sashapush@tut.by>
2024-10-20 13:19:32 +0200

    - added "POST" method to create.jinja2 - app.py uses POST requests payload instead of address bar and nothing is send there.

--- Section 15 flask app/app.py
+++ Section 15 flask app/app.py
@@ -20,17 +20,17 @@ def post(post_id): #better not to name function as variable and vice versa
     if not post: # if post not found - None if not found; not None => True
         return render_template('404.jinja2', message=f"A post with id {post_id} was not found.")
     return render_template('post.jinja2', post=posts.get(post_id)) #we pass post argument from post.jinja2
-    
+
 
 @app.route('/post/form')
 def form():
     return render_template('create.jinja2')
 
-#http://127.0.0.1:5000/post/form?title=zzzzz&content=+aaaa
-@app.route('/post/create')
+
+@app.route('/post/create', methods=['POST']) #we configure so this route will only receive POST requets
 def create():
-    title = request.args.get('title')
-    content = request.args.get('content')
+    title = request.form.get('title') #more secure way of reading form's payload
+    content = request.form.get('content') #more secure way of reading form data
     post_id = len(posts)
     posts[post_id] = {"id":post_id, "title":title, "content":content}
     return redirect(url_for('post', post_id=post_id)) #url_for - take in function name and return url we want. def post() needs post_id and we give it in url_for
