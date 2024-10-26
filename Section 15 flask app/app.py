ff7b8f4459b10b0ea6edaa464216ad0c4ed538b1
Alexander <sashapush@tut.by>
2024-10-26 13:23:07 +0200

    - added home.jinja2 to display in-memory blog entries, which supports dynamic display, not hardcoded

--- Section 15 flask app/app.py
+++ Section 15 flask app/app.py
@@ -1,25 +1,29 @@
 from flask import Flask, render_template, request, redirect, url_for
 
-app = Flask(__name__) #__name__ is always unique
+app = Flask(__name__)  # __name__ is always unique
 
+
+#TODO replace with DB integration
 posts = {
-    0:{
+    0: {
         'post_id': 0,
         'title': 'a title',
         'content': 'This is again the first one'
     }
 }
 
+
 @app.route('/')
 def home():
-    return "Hello, uwu!"
+    return render_template("home.jinja2", posts=posts)
 
-@app.route('/post/<int:post_id>') #crocodile clips, int is replaced by number of post_id. Flask unique syntax
-def post(post_id): #better not to name function as variable and vice versa
+
+@app.route('/post/<int:post_id>')  # crocodile clips, int is replaced by number of post_id. Flask unique syntax
+def post(post_id):  # better not to name function as variable and vice versa
     post = posts.get(post_id)
-    if not post: # if post not found - None if not found; not None => True
+    if not post:  # if post not found - None if not found; not None => True
         return render_template('404.jinja2', message=f"A post with id {post_id} was not found.")
-    return render_template('post.jinja2', post=posts.get(post_id)) #we pass post argument from post.jinja2
+    return render_template('post.jinja2', post=posts.get(post_id))  # we pass post argument from post.jinja2
 
 
 @app.route('/post/form')
@@ -27,16 +31,18 @@ def form():
     return render_template('create.jinja2')
 
 
-@app.route('/post/create', methods=['GET','POST']) #we configure so this route will only receive POST requets
+@app.route('/post/create', methods=['GET', 'POST'])  # we configure so this route will only receive POST requets
 def create():
-    if request.method== "POST":
-        title = request.form.get('title') #more secure way of reading form's payload
-        content = request.form.get('content') #more secure way of reading form data
+    if request.method == "POST":
+        title = request.form.get('title')  # more secure way of reading form's payload
+        content = request.form.get('content')  # more secure way of reading form data
         post_id = len(posts)
-        posts[post_id] = {"id":post_id, "title":title, "content":content}
-        return redirect(url_for('post', post_id=post_id)) #url_for - take in function name and return url we want. def post() needs post_id and we give it in url_for
-        #redirect is wrapping this "url_for" and tells the browser to load not /create/  but redirect to url_for
+        posts[post_id] = {"id": post_id, "title": title, "content": content}
+        return redirect(url_for('post',
+                                post_id=post_id))  # url_for - take in function name and return url we want. def post() needs post_id and we give it in url_for
+        # redirect is wrapping this "url_for" and tells the browser to load not /create/  but redirect to url_for
     return render_template('create.jinja2')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
