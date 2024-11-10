f7c4c5d7b267e7ce211e7b38a4a5525e73080fe8
Alexander <sashapush@tut.by>
2024-11-10 15:55:53 +0100

    Added Proper decorator to work with arguments

--- Section 17 decorators/Example decorator.py
+++ Section 17 decorators/Example decorator.py
@@ -2,30 +2,37 @@ import functools
 # decorator is higher order function (which takes in functions as argument) and they also RETURN a function
 user = {'username': 'jose123', 'access_level': 'admin'}
 # user = {    'username':'jose123', 'access_level': 'guest'}
-def user_has_permission(func):
-    @functools.wraps(func) #tells Python that secure_func() is wrapping around original "func" - to keep original function name and docstring
-    def secure_func(panel): #wrapper function; #needs to have param, since without it my_function() will give 1 param when 0 expected
-        if user.get("access_level") == 'admin':  # obv this code is returned when user level is admin
-            return func(panel)  #same, param should be returned
-    return secure_func
-    # initial way of handling below
-    # if user.get("access_level") == 'admin':
-    #     return func
-    # raise RuntimeError
-@user_has_permission #this decorator removes the need for
-#my_secure_function = user_has_permission(my_function) below
+def third_level(access_level):
+    def user_has_permission(func):
+        @functools.wraps(func) #tells Python that secure_func() is wrapping around original "func" - to keep original function name and docstring
+        def secure_func(panel): #wrapper function; #needs to have param, since without it my_function() will give 1 param when 0 expected
+            if user.get("access_level") == access_level:  # obv this code is returned when user level is admin
+                return func(panel)  #same, param should be returned
+        return secure_func
+    return user_has_permission
+
+
+@third_level('admin') #it's a function call
 def my_function(panel):
     """Allows us to retrieve password for the admin panel"""
-    return f'Password for {panel} panel is 1234.'
-@user_has_permission #now we can't apply the decorator because decorator will pass in "panel" argument
-def another_fuction():
-    return "Hello there"
+    return f'Password {panel} for panel is 1234.'
 
- #my_secure_function = user_has_permission(my_function)
+def another_function():
+    return "Hello there"
 #print(my_function())
 #print(my_function.__name__) #secure_func since it replaces my_function when running through the decorator
 #print(my_function.__doc__) #returns none, instead of "    """Allows us to retrieve passwrod for the admin panel""", because secure_func has no doc
 #print(another_fuction.__name__)
 
-print(my_function('movies'))
-another_fuction() #TODO there's a challenge to use decorator with different functions having different number of arguments
+print(my_function.__name__)
+#print(my_function('movies'))
+print(my_function('user'))
+#another_fuction() #TODO there's a challenge to use decorator with different functions having different number of arguments
+
+user_has_permission = third_level('admin')
+'''we're calling a function, giving it 'access_level' parameter;
+which creates another function "def user_has_permission" and that's our decorator
+decorator acts as normal - takes in a function "func", wraps around it and returns another function "def secure_func"
+and calls original function "func(panel)" with any arguments'''
+
+#my_function = third_level('admin')(my_function)
