20fdc9a50d619b11c0e892606bb0960b316cceb4
Alexander <sashapush@tut.by>
2025-03-08 11:56:43 +0100

    Start on tkinter app - added input field and stored data from it

--- Section  19 tkinter/app.py
+++ Section  19 tkinter/app.py
@@ -3,12 +3,19 @@ from tkinter import ttk
 #tk._test()
 
 def greet():
-    print("Hello world")
+    print(f"Hello {user_name.get() or 'World'}") #.get() is called on tk.StringVar object to retrieve the contents of the variable
 
 
 
 root = tk.Tk() #create Tk object from tk package
 root.title("Hello")
+user_name = tk.StringVar() #object used to track entry widget
+
+name_label = ttk.Label(root,text="Name:")
+name_label.pack(side="left",padx=(0,10))
+name_entry = ttk.Entry(root, width=15,textvariable=user_name)
+name_entry.pack(side="left")
+name_entry.focus()
 
 greet_button = ttk.Button(root,text="Greetings", command=greet) #(where to put it, and what to run)
 greet_button.pack(side='left', fill="x", expand=True)
