88008cbb66e9b27140ddb2378d008a218cb088d9
Alexander <sashapush@tut.by>
2025-07-26 13:17:03 +0200

    added more unit tests examples

---
+++ Section 20 unit testing/page.py
@@ -0,0 +1,9 @@
+#patching and moking
+import requests
+
+class PageRequester:
+    def __init__(self, url: str):
+        self.url = url
+
+    def get(self):
+        return requests.get(self.url).content
